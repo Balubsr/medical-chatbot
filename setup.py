545aa6d3b94f2be5a32614b42cbd2e5482a7cc69
AnkammaRao-G <ak.gunji09@gamil.com>
2025-12-27 20:56:48 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medical_chatbot',
+    version="0.1.0",
+    author='G.AnkammaRao',
+    author_email='balubsr26@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+    
+
+
+)
